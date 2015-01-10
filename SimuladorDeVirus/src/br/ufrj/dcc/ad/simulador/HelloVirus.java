9b900aa1d57bfd6888eb9fa129eb8d4c89fd04ea
Pedro <pedrohcb.ufrj@gmail.com>
2015-01-10 17:23:57 -0200

    Minor changes / Initial commit / no title

--- SimuladorDeVirus/src/br/ufrj/dcc/ad/simulador/HelloVirus.java
+++ SimuladorDeVirus/src/br/ufrj/dcc/ad/simulador/HelloVirus.java
@@ -1,5 +1,24 @@
-package br.ufrj.dcc.ad.simulador;
+/************************************************************************************
+ * Main class of the project. It expects 3 doubles and 1 integer as arguments.
+ * - First Argument -> r4 value
+ * - Second Argument -> delta value
+ * - Third Argument -> min_r4 value
+ * - Fourth Argument -> Analysis Type
+ * 
+ * Analysis Types :
+ * 1 - runRingCostAnalysis();
+ * 2 - runMeshOnce();
+ * 3 - runMeshCostAnalysis();
+ * 4 - runEndogenousMeshCostAnalysis();
+ * 5 - runEndogenousRingCostAnalysis();
+ * 6 - runSingleNodeCostAnalysis();
+ * 7 - runSingleNodeTimeAnalysis();
+ * 
+ * @author bkosawa, ffaria, pedrohcb, rney
+ * @date January 13, 2015 
+ ************************************************************************************/
 
+package br.ufrj.dcc.ad.simulador;
 import br.ufrj.dcc.ad.simulador.interfaces.VirusSimulation;
 import br.ufrj.dcc.ad.simulador.model.PrintOptions;
 import br.ufrj.dcc.ad.simulador.model.Rates;
@@ -9,6 +28,7 @@ import br.ufrj.dcc.ad.simulador.utils.Statistics;
 
 import java.text.DecimalFormat;
 import java.util.Map;
+import java.util.Scanner;
 
 public class HelloVirus {
 
@@ -25,32 +45,93 @@ public class HelloVirus {
 	static Double r4 = 1.0;
 	static Double delta = 0.01;
 	static Double min_r4 = 0.0;
+	static int analysis_type = 6;
 	
 	static int maxEvents = 10000;
 	
-//	public static Printer printer = new Printer();
-
 	public static void main(String[] args) {
-//		printer = new Printer();
 		
-		// System.out.println("Welcome to the best assignment in the world!");
-
-		if (args.length == 3) {
-			r4 = Double.parseDouble(args[0]);
-			delta = Double.parseDouble(args[1]);
-			min_r4 = Double.parseDouble(args[2]);
+		System.out.println("================ Welcome to the best assignment in the world ================");
+		
+		if (args.length == 4) {
+			try {
+				r4 = Double.parseDouble(args[0]);
+				delta = Double.parseDouble(args[1]);
+				min_r4 = Double.parseDouble(args[2]);
+				analysis_type = Integer.parseInt(args[4]);
+				
+			} catch(Exception e) {
+				System.out.println("Wrong parameters! Error: " + e);
+				System.out.println("Closing the program");
+				return;
+			}
+		} else {
+			boolean ready = false;
+			Scanner keyboard = new Scanner(System.in);
+			System.out.println("WARNING! You did not start your program with the right parameters!");
+			while (!ready) {
+				try {
+					System.out.println("Please chose your r4 **double value** (eg: 1,0) :");
+					r4 = keyboard.nextDouble();
+					System.out.println("Now chose your delta **double value** (eg: 0,01) :");
+					delta = keyboard.nextDouble();
+					System.out.println("Now chose your min_r4 **double value** (eg: 0,0) :");
+					min_r4 = keyboard.nextDouble();
+					System.out.println("And for last, your analysis_type (choose an integer number): " + 
+										"\n 1 - runRingCostAnalysis" +
+					 					"\n 2 - runMeshOnce" +
+					 					"\n 3 - runMeshCostAnalysis" +
+					 					"\n 4 - runEndogenousMeshCostAnalysis" +
+					 					"\n 5 - runEndogenousRingCostAnalysis" +
+					 					"\n 6 - runSingleNodeCostAnalysis" +
+					 					"\n 7 - runSingleNodeTimeAnalysis"
+					 					); 
+					analysis_type = keyboard.nextInt();
+					ready = true;
+				} catch(Exception e) {
+					System.out.println("An error has occurred! Error: " + e);
+					System.out.println("Make sure that your double input is using comma instead of dot for the decimal part!");
+					System.out.println("****************************** Try Again ********************************");
+					keyboard.next();
+				}
+			}
+			keyboard.close();
 		}
+		System.out.println("Parameters...");
+		System.out.println("r4 : " + r4);
+		System.out.println("delta : " + delta);
+		System.out.println("min_r4 : " + min_r4);
+		System.out.println("analysis_type: " + analysis_type);
+		System.out.println("====================== Thanks! Starting the program! =======================");
 		
-//		runRingCostAnalysis();
-//		runMeshOnce();
-//		runMeshCostAnalysis();
-		runMeshTimeAnalysis(0.009999999999999247);
-//      runEndogenousMeshCostAnalysis();
-//		runEndogenousRingCostAnalysis();
-//		runSingleNodeCostAnalysis();
-//		runSingleNodeTimeAnalysis();
-
-		System.out.println("Finished");
+		switch (analysis_type) {
+		case 1:
+			runRingCostAnalysis();
+			break;
+		case 2:
+			runMeshOnce();
+			break;
+		case 3:
+			runMeshCostAnalysis();
+			break;
+		case 4:
+			runEndogenousMeshCostAnalysis();
+			break;
+		case 5:
+			runEndogenousRingCostAnalysis();
+			break;
+		case 6:
+			runSingleNodeCostAnalysis();
+			break;
+		case 7:
+			runSingleNodeTimeAnalysis(r4);
+			break;	
+		default:
+			System.out.println("Wrong Analysis type!! CLOSING THE PROGRAM!!");
+			return;
+		}
+		System.out.println("====================== .CSVs GENERATED ON ROOT FOLDER ======================");
+		System.out.println("================================= Finished =================================");
 	}
 
 	static void runEndogenousRingCostAnalysis() {
@@ -213,7 +294,6 @@ public class HelloVirus {
 
 
 	static void runSingleNodeCostAnalysis() {
-		System.out.println("R4;piO;cV;cS;cT");
 		FileUtil file = new FileUtil("CostAnalysis.csv", "R4;piO;cV;cS;cT");
 		double bestR4 = r4;
 		double bestTotalCost = REALY_LARGE_NUM;
@@ -268,7 +348,6 @@ public class HelloVirus {
 			Statistics.accumulateTotalCost(stats.getTotalCost());
 			Statistics.accumulatePrePDF(stats.getPrePDF(), stats.getTotalCount());
 		}
-
 
 		printer.printGlobalCDF(file, r4, Statistics.getGlobalCDF());
 		Statistics.resetGlobalStatistics();
